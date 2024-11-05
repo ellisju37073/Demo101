ad94945c632de1a5259b3df629f59357c57496a3
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2024-11-05 11:34:45 -0800

    update

--- FactoryModel/Program.cs
+++ FactoryModel/Program.cs
@@ -15,7 +15,43 @@ namespace FactoryModel
             Console.WriteLine($"Provider: {provider}");
             Console.WriteLine($"ConnectionString: {connectionString}");
 
+            DbProviderFactory factory = GetDbProviderFactory(provider);
 
+            using (DbConnection connection = factory.CreateConnection())
+            {
+               Console.WriteLine($"Connection Type: {connection.GetType().Name}");
+
+               connection.ConnectionString = connectionString;
+               connection.Open();
+
+                DbCommand command = factory.CreateCommand();
+                Console.WriteLine($"Command Type: {command.GetType().Name}");
+
+                command.Connection = connection;
+
+                command.CommandText = "Select i.Id, m.Name From Inventory i inner join Makes m on m.Id = i.MakeId";
+                using (DbDataReader reader = command.ExecuteReader())
+                {
+                  Console.WriteLine($"Id \t Your data reader object is a : {reader.GetType().Name}");
+
+                  Console.WriteLine("\n\t *** Current Inventory ***");
+
+                    while(reader.Read()) {
+                        Console.WriteLine($"\t-> Car # {reader["Id"]} is a {reader["Name"]}");
+                    }
+                }
+            }
+
+
+        }
+
+        static DbProviderFactory GetDbProviderFactory(string provider)
+        {
+            if (provider == "SqlServer")
+            {
+                return SqlClientFactory.Instance;
+            }
+            else return null;
         }
         static (string Provider, string ConnectionString)
 
