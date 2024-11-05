b48751f811eea79d6b612ee5a3abcfd2eee97024
ellisju37073 <67281488+ellisju37073@users.noreply.github.com>
2024-11-05 11:13:55 -0800

    Add project files.

---
+++ FactoryModel/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+
+
+namespace FactoryModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Factory Model Example");
+
+            var (provider, connectionString) = GetProviderFromConfiguation();
+            Console.WriteLine($"Provider: {provider}");
+            Console.WriteLine($"ConnectionString: {connectionString}");
+
+
+        }
+        static (string Provider, string ConnectionString)
+
+            GetProviderFromConfiguation()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+            var providerName = configuration["ProviderName"];
+            var connectionString = configuration[$"{providerName}:ConnectionString"];
+            return (providerName, connectionString);
+        }
+}
+}
